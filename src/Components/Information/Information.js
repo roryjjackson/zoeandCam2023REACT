57442838be87e647b018520f7bef103224568b2d
rory jackson <roryjamesjackson@hotmail.com>
2023-03-27 16:11:36 +0100

    owner tweaks, p tag size and organisation of text

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -22,6 +22,9 @@ export default function Information() {
         <div className="divider">
           <p>Welcome! We may already be legally wed, but that doesn't mean we're missing the opportunity to celebrate with our nearest and dearest!</p>
           < Countdown />
+          <div>
+            <audio className="music-player" controls src="https://res.cloudinary.com/dfipoufmj/video/upload/v1675534541/This_Will_Be_An_Everlasting_Love_mutyct.mp3"></audio>
+          </div>
         </div>
 
 
@@ -48,7 +51,7 @@ export default function Information() {
         <div className="section">
           <div className="block-content block-left">
             <h3>Travel</h3>
-            <p>We have reserved a number of rooms at Travelodge Broxden (10 mins from our venue). To book this, just fill in the form by clicking the button below.</p>
+            <p>We have reserved a number of rooms at Travelodge Broxden at a discounted rate (10 mins from our venue). To book this, just fill in the form by clicking the button below.</p>
             <button onClick={ e => {handleClick(); setButtonPopup(true)}} className="button">Book Travelodge</button>
             <br/>
             <p>The venue has a list of local suggestions, but you should also be able to find plenty of options via booking.com or a good old fashioned Google search.</p>
@@ -76,12 +79,14 @@ export default function Information() {
         <div className="section" id="things-to-note">
           <img src="https://res.cloudinary.com/dfipoufmj/image/upload/v1669048233/zoe_header_pic_q6rlbj.jpg" alt="" className="block-left"></img>
           <div className="block-content block-right">
-            <h3>Things to note</h3>
-            <p>DRESS CODE: Formal and Fancy (umbrellas strongly advised)</p>
-            <p>Adult only affair</p>
+            <h3 id="things-to-note-header">Things to note</h3>
+            <p>DRESS CODE: <span id="formal-and-fancy">Formal and Fancy</span> (umbrellas strongly advised)</p>
+            <p>*Adult only affair*</p>
             <p>Any questions, email zoeandcam2023@gmail.com</p>
             <p>Mealtimes and toasts are on us. Party drinks are on you!</p>
-            <p>* If you prefer something a little stronger than vino, we will have a semi-subsidised bar that accepts all major debit / credit cards. No naughty hipflasks or outside beverages please - it interferes with licensing and will shut down our throwdown!</p>
+            <br></br>
+            <p className="small-p-tag">* If you prefer something a little stronger than vino, we will have a semi-subsidised bar that accepts all major debit / credit cards.</p>
+            <p className="small-p-tag">No naughty hipflasks or outside beverages please - it interferes with licensing and will shut down our throwdown!</p>
           </div>
         </div>
 
